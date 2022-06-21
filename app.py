dbbde8075c88b9b747df9acc2e0f0099965c3aea
Alex Bush <alexbush@developmentseed.org>
2022-06-21 13:16:14 +0100

    feat: add eks cluster and vpc

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 """ CDK Configuration for the nasa-analytics stack."""
 
-from aws_cdk import App, Stack, Tags, aws_iam
+from aws_cdk import App, Stack, Tags, aws_iam,DefaultStackSynthesizer
 from constructs import Construct
+from cluster.construct import ClusterConstruct
 
 app = App()
 
 
-class NasaAnalyticsStack(Stack):
+class AnalyticsStack(Stack):
     """CDK stack for the nasa-analytics stack."""
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         """."""
         super().__init__(scope, construct_id, **kwargs)
 
-analytics_stack = AnalyticsStack( app, "analytics-stack")
+analytics_stack = AnalyticsStack( app, "analytics-stack",synthesizer=DefaultStackSynthesizer(qualifier="analytics"))
+
+cluster = ClusterConstruct(analytics_stack, "cluster")
 
 for key, value in {
     "Project": "NASA Analytics",
--- cluster/construct.py
+++ cluster/construct.py
@@ -1,33 +1,25 @@
-"""CDK Constrcut for a Lambda based TiTiler API with pgstac extension."""
+"""CDK Construct for a EKS based Kubernetes cluster."""
 import os
 
 from aws_cdk import (
     CfnOutput,
     Duration,
     Stack,
-    aws_apigatewayv2_alpha,
-    aws_apigatewayv2_integrations_alpha,
+    aws_eks,
     aws_ec2,
-    aws_iam,
-    aws_lambda,
-    aws_logs,
 )
 from constructs import Construct
 
-from .config import delta_raster_settings
 
 
-class RasterApiLambdaConstruct(Construct):
-    """CDK Constrcut for a Lambda based TiTiler API with pgstac extension."""
+class ClusterConstruct(Construct):
+    """CDK Construct for a EKS based Kubernetes cluster."""
 
     def __init__(
         self,
         scope: Construct,
         construct_id: str,
-        vpc,
-        database,
         code_dir: str = "./",
-        domain_name: aws_apigatewayv2_alpha.DomainName = None,
         **kwargs,
     ) -> None:
         """."""
@@ -36,69 +28,13 @@ class RasterApiLambdaConstruct(Construct):
         # TODO config
         stack_name = Stack.of(self).stack_name
 
-        delta_raster_function = aws_lambda.Function(
-            self,
-            "lambda",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            code=aws_lambda.Code.from_docker_build(
-                path=os.path.abspath(code_dir),
-                file="raster_api/runtime/Dockerfile",
-                platform="linux/amd64",
-            ),
+        vpc = aws_ec2.Vpc(
+                        self,
+                        "vpc-test"
+                    )
+
+        aws_eks.Cluster(self, "HelloEKS",
+            version=aws_eks.KubernetesVersion.V1_21,
             vpc=vpc,
-            allow_public_subnet=True,
-            handler="handler.handler",
-            memory_size=delta_raster_settings.memory,
-            timeout=Duration.seconds(delta_raster_settings.timeout),
-            log_retention=aws_logs.RetentionDays.ONE_WEEK,
-            environment=delta_raster_settings.env or {},
-        )
-
-        database.pgstac.secret.grant_read(delta_raster_function)
-        database.pgstac.connections.allow_from(
-            delta_raster_function, port_range=aws_ec2.Port.tcp(5432)
-        )
-
-        delta_raster_function.add_environment(
-            "DELTA_RASTER_ENABLE_MOSAIC_SEARCH",
-            str(delta_raster_settings.enable_mosaic_search),
-        )
-
-        delta_raster_function.add_environment(
-            "DELTA_RASTER_PGSTAC_SECRET_ARN", database.pgstac.secret.secret_full_arn
-        )
-
-        raster_api_integration = (
-            aws_apigatewayv2_integrations_alpha.HttpLambdaIntegration(
-                construct_id, delta_raster_function
-            )
-        )
-
-        domain_mapping = None
-        if domain_name:
-            domain_mapping = aws_apigatewayv2_alpha.DomainMappingOptions(
-                domain_name=domain_name
-            )
-
-        self.raster_api = aws_apigatewayv2_alpha.HttpApi(
-            self,
-            f"{stack_name}-{construct_id}",
-            default_integration=raster_api_integration,
-            default_domain_mapping=domain_mapping,
-        )
-
-        CfnOutput(self, "raster-api", value=self.raster_api.url)
-
-        delta_raster_function.add_to_role_policy(
-            aws_iam.PolicyStatement(
-                actions=["s3:GetObject"],
-                resources=[
-                    f"arn:aws:s3:::{bucket}/{delta_raster_settings.key}"
-                    for bucket in delta_raster_settings.buckets
-                ],
-            )
-        )
-
-        database.pgstac.connections.allow_from(
-            delta_raster_function, port_range=aws_ec2.Port.tcp(5432)
+            vpc_subnets=[aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_NAT)]
         )
