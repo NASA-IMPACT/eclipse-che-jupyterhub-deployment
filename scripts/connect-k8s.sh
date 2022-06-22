6d8e65823362d2b310a8b8fc4cb35b5482300a27
Alex Bush <alexbush@developmentseed.org>
2022-06-22 19:30:30 +0100

    feat: add cluster admin role; feat: add k8s connect script

--- scripts/connect-k8s.sh
+++ scripts/connect-k8s.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 CLUSTER_NAME=$(aws eks list-clusters --query clusters --output text | grep analyticscluster)
 echo $CLUSTER_NAME
-aws eks update-kubeconfig --region us-east-1 --name $CLUSTER_NAME
-aws eks get-token --region us-east-1 --cluster-name $CLUSTER_NAME
+CONFIG=$(aws cloudformation describe-stacks --stack-name analytics-stack --query 'Stacks[0].Outputs[?contains(OutputKey,`ConfigCommand`)].OutputValue' --output=text)
+TOKEN=$(aws cloudformation describe-stacks --stack-name analytics-stack --query 'Stacks[0].Outputs[?contains(OutputKey,`TokenCommand`)].OutputValue' --output=text)
+$CONFIG
+$TOKEN
