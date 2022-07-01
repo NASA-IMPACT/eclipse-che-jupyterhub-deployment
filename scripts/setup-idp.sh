cdd85c49f68feaa5a30b14aa8918ff137ccb40f3
Alex Bush <alexbush@developmentseed.org>
2022-07-01 11:10:59 +0100

    fix: add IDP overwrite check

---
+++ scripts/setup-idp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+CLUSTER_NAME=$(aws eks list-clusters --query clusters --output text | grep analyticscluster)
+echo "Cluster name is $CLUSTER_NAME"
+IDP_COUNT=$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc" --output text | wc -l)
+echo "Found $IDP_COUNT IDPs"
+if [ $IDP_COUNT -eq 0 ]; then
+  eksctl associate identityprovider -f eks/associate-identity-provider.yaml
+fi
