f67228a099f32a242f908c4feffde232558b0ab0
Alex Bush <alexbush@developmentseed.org>
2022-07-04 12:26:10 +0100

    fix: leftover test

--- scripts/setup-idp.sh
+++ scripts/setup-idp.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 CLUSTER_NAME=$(aws eks list-clusters --query clusters --output text | grep analyticscluster)
 echo "Cluster name is $CLUSTER_NAME"
-IDP_COUNT=0
-$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc" --output text | wc -l)
+IDP_COUNT=$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc" --output text | wc -l)
 echo "Found $IDP_COUNT IDPs"
 if [ $IDP_COUNT -eq 0 ]; then
   eksctl associate identityprovider -f eks/associate-identity-provider.yaml
