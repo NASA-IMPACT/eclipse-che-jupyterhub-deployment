43c9b7bca43af44b304f49ca5a13b720ab34e2ad
Alex Bush <alexbush@developmentseed.org>
2022-07-04 12:25:52 +0100

    fix: mess

--- scripts/setup-idp.sh
+++ scripts/setup-idp.sh
@@ -2,7 +2,7 @@
 CLUSTER_NAME=$(aws eks list-clusters --query clusters --output text | grep analyticscluster)
 echo "Cluster name is $CLUSTER_NAME"
 IDP_COUNT=0
-#$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc" --output text | wc -l)
+$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc" --output text | wc -l)
 echo "Found $IDP_COUNT IDPs"
 if [ $IDP_COUNT -eq 0 ]; then
   eksctl associate identityprovider -f eks/associate-identity-provider.yaml
