42b1e63982115c3d78827bd706c406ff3aae418a
Timur Girgin <girgin.timur@gmail.com>
2022-07-05 15:34:39 +0200

    fix: path to script

--- scripts/install-che.sh
+++ scripts/install-che.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PLATFORM=$(uname -s)
 CHECTL_VERSION=7.49.0
-curl -sL "https://github.com/che-incubator/chectl/releases/download/${CHECTL_VERSION}/chectl-${PLATFORM}-x64.tar.gz" > che-install.sh
+curl -sL "https://github.com/che-incubator/chectl/releases/download/${CHECTL_VERSION}/chectl-${PLATFORM}-x64.tar.gz" > ./scripts/che-install.sh
 chmod +x ./scripts/che-install.sh
-./che-install.sh
+./scripts/che-install.sh
