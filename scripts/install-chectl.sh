6ddd4b4df4572f0d7ce1e7058a4e86bc0a385d0f
Timur Girgin <girgin.timur@gmail.com>
2022-07-05 18:05:17 +0200

    chore: add chectl to /usr/local/bin

--- scripts/install-chectl.sh
+++ scripts/install-chectl.sh
@@ -2,4 +2,4 @@
 PLATFORM=$(uname -s)
 CHECTL_VERSION=7.49.0
 curl -sL "https://github.com/che-incubator/chectl/releases/download/${CHECTL_VERSION}/chectl-${PLATFORM}-x64.tar.gz" | tar -xz -C /tmp
-alias chectl=/tmp/chectl/bin/run
+mv /tmp/chectl/bin/run /usr/local/bin
