b90f0a51b3c887c9d07a2e3dcd9a699e4ff55b82
Timur Girgin <girgin.timur@gmail.com>
2022-07-05 15:45:26 +0200

    fix: use chectl bin

--- scripts/install-che.sh
+++ scripts/install-che.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 PLATFORM=$(uname -s)
 CHECTL_VERSION=7.49.0
-curl -sL "https://github.com/che-incubator/chectl/releases/download/${CHECTL_VERSION}/chectl-${PLATFORM}-x64.tar.gz" > ./scripts/che-install.sh
-chmod +x ./scripts/che-install.sh
-./scripts/che-install.sh
+curl -sL "https://github.com/che-incubator/chectl/releases/download/${CHECTL_VERSION}/chectl-${PLATFORM}-x64.tar.gz" | tar -xz -C /tmp
+alias chectl=/tmp/chectl/bin/run
