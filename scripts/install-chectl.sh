c2ac8db290d4cc963362a50b91825d7e9ec094b2
Alex Bush <alexbush@developmentseed.org>
2022-07-07 21:57:00 +0100

    feat: increase local build speed (#54)
    fix: build speed

--- scripts/install-chectl.sh
+++ scripts/install-chectl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ command -v chectl &> /dev/null ]; then
+    echo "Chectl already installed"
+    exit
+fi
 curl -sL  https://www.eclipse.org/che/chectl/ > che-install.sh
 chmod +x ./che-install.sh
 ./che-install.sh
