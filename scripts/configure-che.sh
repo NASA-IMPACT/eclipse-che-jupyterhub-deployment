afba2ee596d033fdaf0a165aa80422250be44cd7
Alex Bush <alexbush@developmentseed.org>
2022-07-07 21:01:45 +0100

    fix: actually fix the DNS certmanager thing

--- scripts/configure-che.sh
+++ scripts/configure-che.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 chectl server:deploy --platform k8s --che-operator-cr-patch-yaml=operator-patch.yaml --domain "${QUALIFIER}-analytics.delta-backend.com" --skip-oidc-provider-check --telemetry=off
 
-CERTMANAGER_KEY_ID=$(aws secretsmanager get-secret-value --secret-id "certmanager-accesskeyid-${QUALIFIER}" --query SecretString --output text)
-CERTMANAGER_SECRET=$(aws secretsmanager get-secret-value --secret-id "certmanager-accesskey-secret-${QUALIFIER}" --query SecretString --output text)
+export CERTMANAGER_KEY_ID=$(aws secretsmanager get-secret-value --secret-id "certmanager-accesskeyid-${QUALIFIER}" --query SecretString --output text)
+export CERTMANAGER_SECRET=$(aws secretsmanager get-secret-value --secret-id "certmanager-accesskey-secret-${QUALIFIER}" --query SecretString --output text)
 
 envsubst < eks/aws-cert-manager-access-key.secret.yaml > eks/secrets-substs.yaml
 kubectl apply -f eks/secrets-substs.yaml
