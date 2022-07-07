6d04c52c3208e58bd35d65b3272c30284b801dba
Timur Girgin <girgin.timur@gmail.com>
2022-07-07 18:49:45 +0200

    feat: add dns record delete command

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 scripts/remove-cert-manager-policy.sh
+export ROUTE53_ACTION="DELETE"; scripts/set-dns-record.sh
 kubectl get services -n ingress-nginx -o yaml > delete-namespace.yaml
 kubectl delete -f delete-namespace.yaml
 cdk destroy --qualifier ${QUALIFIER} --toolkit-stack-name ${QUALIFIER}
