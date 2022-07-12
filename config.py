ac5495e10bdf66f20a2448922549936204837064
Timur Girgin <girgin.timur@gmail.com>
2022-07-12 13:20:42 +0200

    refactor: removed unused env variables

--- config.py
+++ config.py
@@ -6,13 +6,9 @@ class Settings(pydantic.BaseSettings):
     stack_tag: str = "dev-stack"
     client_tag: str = "nasa-impact"
     owner_tag: str = "ds"
-    qualifier: str
-    idp_url: str
-    idp_user_claim: str
+    qualifier: str = 'analysis'
     auth_client_name: str = "not_random_auth_client_name"
     auth_secret: str = "not_random_auth_secret"
-    aws_access_key_id: str
-    aws_secret_access_key: str
     cluster_region: str = "us-west-2"
 
     class Config:
